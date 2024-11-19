ca8fed4a5a9aabbdf82d29703f8332f9542f20c4
Pykou <pykou1228@gmail.com>
2024-11-19 23:53:28 +0800

    index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Welcome to GCC","Spooky! 🎃","Happy Halloween!🎃"];
+const statusMessages = ["🎁 Sending Rewards","🎁 Giveaway at 100 members!"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
